461746d4b1f7a3f8f9d5ca03a77546d188ffbfbf
Arivan199 <63254290+Arivan199@users.noreply.github.com>
2020-04-25 17:00:23 +0530

    Update automation1.sh

--- automation1.sh
+++ automation1.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-#automate the updation for autoschedule
-for i in {1..50..1}
-do
-sudo echo "00 00 * * * /bin/sh /home/Army$i/autoschedule.sh"|cat >> /etc/crontab
-sudo echo "00 00 * * * /bin/sh /home/Navy$i/autoschedule.sh"|cat >> /etc/crontab
-sudo echo "00 00 * * * /bin/sh /home/AirForce$i/autoschedule.sh"|cat >> /etc/crontab
-done
-#
+
