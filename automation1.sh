b715c6a4edfb7560794ffcdc95b58d8e07a4bac7
Arivan199 <63254290+Arivan199@users.noreply.github.com>
2020-04-25 16:39:16 +0530

    Update and rename automation.sh to automation1.sh

--- automation1.sh
+++ automation1.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#automate the updation everyday for 
-sudo echo "00 06 * * * /bin/sh /home/ArmyGeneral/attendance.sh"|cat >> /etc/crontab
-sudo echo "00 06 * * * /bin/sh /home/NavyMarshal/attendance.sh"|cat >> /etc/crontab
-sudo echo "00 06 * * * /bin/sh /home/AirForceChief/attendance.sh"|cat >> /etc/crontab
-#
 #automate the updation for autoschedule
 for i in {1..50..1}
 do
