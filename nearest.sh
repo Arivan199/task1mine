30c6a52d0cae5bdf33983b414202cd18b106dc97
Arivan199 <63254290+Arivan199@users.noreply.github.com>
2020-04-30 20:10:26 +0530

    Update nearest.sh

--- nearest.sh
+++ nearest.sh
@@ -12,30 +12,26 @@
 #therefore nearest ten posts are reported
 sudo touch /home/ChiefCommander/nearest10
 declare -r PI=3.14
-dbt($lng1,$lng2,$lat1,$lat2)
-{
-	$teta =   $lng1 - $lng2;
-	$distance =   Math.sin(deg2rad($lat1))*Math.sin(deg2rad($lat2))+Math.cos(deg2rad($lat1))*Math.cos(deg2rad($lat2))*Math.cos(deg2rad($teta));
-	$distance =   $dist * 60;
-	$distance =   $dist * 1852;
-	return ($dist);
-}
-deg2rad($x)
-{
-	return ($x*PI/180);
-}
-rad2deg($x)
-{
-	return ($x*180/PI);
-}
 lng1=77.1025;
 lat1=28.7041;
 for i in {1..50..1}
 do
- x=grep -w Army$i /home/ArmyGeneral/Attendance.txt|awk '{print $2;}'
- lng2=curl https://inductions.delta.nitt.edu/sysad-task1-position.log|grep x|awk '{print $4;}'|sed 's/E°//';
- lat2=curl https://inductions.delta.nitt.edu/sysad-task1-position.log|grep x|awk '{print $3;}'|sed 's/N°//';
- echo "Army$i"|cat > /home/ChiefCommander/nearest10
- dbt($lng1,$lng2,$lat1,$lat2)|cat > /home/ChiefCommander/nearest10
+ d=$(date +%F)
+ a=$(grep -w $d /home/ArmyGeneral/Attendance_record.txt|grep -w Army$i |awk '{print $2;}')
+ lng2=$(curl https://inductions.delta.nitt.edu/sysad-task1-position.log|grep -w $d|grep -w $a|awk '{print $4;}'|sed 's/E°//');
+ lat2=$(curl https://inductions.delta.nitt.edu/sysad-task1-position.log|grep -w $d|grep -w $a|awk '{print $3;}'|sed 's/N°//');
+ teta=$(echo "$lng1 - $lng2"|bc)
+ dtrlat1=$(echo "$lat1 * PI / 180|bc -l")
+ dtrlat2=$(echo "$lat2 * PI / 180|bc -l")
+ dtrteta=$(echo "$teta * PI / 180|bc -l")
+ sinlat1=$(bc -l <<< "s($dtrlat1)")
+ sinlat2=$(bc -l <<< "s($dtrlat2)")
+ coslat1=$(bc -l <<< "c($dtrlat1)")
+ coslat2=$(bc -l <<< "s($dtrlat2)")
+ costeta=$(bc -l <<< "s($dtrteta)")
+ distance=$(echo "$sinlat1 * $sinlat2 + $coslat1 * $coslat2 * $costeta|bc -l")
+ distance=$(echo "$distance * 60|bc -l")
+ distance=$(echo "$distance * 1852|bc -l")
+ echo "Army$i   $distance"|column -t|cat >> /home/ChiefCommander/nearest10
 done
 sort -k2 -n /home/ChiefCommander/nearest10|head
